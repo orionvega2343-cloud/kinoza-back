868e8ad76d9a9dd7b3d360133d084b0fc91ec438
van1dal <van1dalgrr@gmail.com>
2026-09-18 18:30:01 +0300

    feat(watchlist): add create service

--- internal/watchlist/service/favorites_service.go
+++ internal/watchlist/service/favorites_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"context"
+
+	"kinoza-back/internal/watchlist/domain"
 	"kinoza-back/internal/watchlist/repository"
 )
 
@@ -8,8 +11,13 @@ type WatchlistService struct {
 	repo repository.WatchlistRepository
 }
 
+// Constructor
 func NewWatchlistService(repo repository.WatchlistRepository) *WatchlistService {
 	return &WatchlistService{
 		repo: repo,
 	}
+}
+
+func (s *WatchlistService) Create(ctx context.Context, item *domain.WatchlistItem) error {
+	return s.repo.Create(ctx, item)
 }
