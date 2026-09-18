d421e23cf577335587d616e595ea324b2555c776
van1dal <van1dalgrr@gmail.com>
2026-09-18 20:36:37 +0300

    feat(watchlist): add handler constructor

--- internal/watchlist/handler/favorites_handler.go
+++ internal/watchlist/handler/favorites_handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"kinoza-back/internal/watchlist/service"
+)
+
+type WatchlistHandler struct {
+	service *service.WatchlistService
+}
+// Constructor
+func NewWatchlistHandler(service *service.WatchlistService) *WatchlistHandler {
+	return &WatchlistHandler{
+		service: service,
+	}
+}
+
+
