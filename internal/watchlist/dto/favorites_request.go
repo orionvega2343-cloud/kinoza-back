d0d7d361dde7e518a6ee59bde4a1d45d4a867b27
van1dal <van1dalgrr@gmail.com>
2026-09-18 22:15:58 +0300

    chore: add gin dependencies

--- internal/watchlist/dto/favorites_request.go
+++ internal/watchlist/dto/favorites_request.go
@@ -1,12 +1,10 @@
 package dto
 
 type CreateWatchlistRequest struct {
-	TitleID uint `json:"title_id"`
-	Status string `json:"status"`
+	TitleID uint   `json:"title_id"`
+	Status  string `json:"status"`
 }
 
 type UpdateWatchlistRequest struct {
 	Status string `json:"status"`
 }
-
-
