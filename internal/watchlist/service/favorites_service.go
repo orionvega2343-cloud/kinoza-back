b923f94a04a603afa903337414d827038878387d
van1dal <van1dalgrr@gmail.com>
2026-09-18 18:24:51 +0300

    feat(watchlist): add service structure

--- internal/watchlist/service/favorites_service.go
+++ internal/watchlist/service/favorites_service.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"kinoza-back/internal/watchlist/repository"
+)
+
+type WatchlistService struct {
+	repo repository.WatchlistRepository
+}
+
+func NewWatchlistService(repo repository.WatchlistRepository) *WatchlistService {
+	return &WatchlistService{
+		repo: repo,
+	}
+}
