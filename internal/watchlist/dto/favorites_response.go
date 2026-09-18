60a91e844e1cc412472a35a8bac4ae1cd015c371
van1dal <van1dalgrr@gmail.com>
2026-09-18 19:11:44 +0300

    feat(watchlist): add create response dto

--- internal/watchlist/dto/favorites_response.go
+++ internal/watchlist/dto/favorites_response.go
@@ -1,1 +1,12 @@
 package dto
+
+import (
+	"time"
+)
+
+type CreateWatchlistResponse struct {
+	ID      uint      `json:"id"`
+	TitleID uint      `json:"title_id"`
+	Status  string    `json:"status"`
+	AddedAt time.Time `json:"added_at"`
+}
