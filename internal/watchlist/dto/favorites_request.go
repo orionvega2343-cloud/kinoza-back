53a005d6a6ef1d06ec4dc809b3911b833c416238
van1dal <van1dalgrr@gmail.com>
2026-09-18 20:28:11 +0300

    feat(watchlist): add request dtos

--- internal/watchlist/dto/favorites_request.go
+++ internal/watchlist/dto/favorites_request.go
@@ -1,1 +1,12 @@
 package dto
+
+type CreateWatchlistRequest struct {
+	TitleID uint `json:"title_id"`
+	Status string `json:"status"`
+}
+
+type UpdateWatchlistRequest struct {
+	Status string `json:"status"`
+}
+
+
